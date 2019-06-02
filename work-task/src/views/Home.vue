ca7218b9a9fe07bc6924d2fc14495eadafacd086
mrfong <therealshadix@gmail.com>
2019-06-02 16:22:51 +0300

    modifded home.vue page

--- work-task/src/views/Home.vue
+++ work-task/src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>Search For Comments or Posts</h1>
+    <input>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+ <style lang="sass" >
+body
+  font-family: Arial, sans-serif
+  background: url("http://www.inggez.com/img/bg-pattern.png"), linear-gradient(to left, #35495E, #47B882)
+  background-attachment: fixed
+  font-size: 16px
+  color: #171a1c
+  line-height: 1.5
 
-export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
-};
-</script>
+input
+  font-size: 18px
+  outline: none
+  padding: 80px
+  display: inline-block
+  width: 60%
+  border-radius: 6px
+  padding: 16px 70px 8px 30px
+  position: relative
+  background: #fff
+  border: 1px solid #ddd
+
+</style>
