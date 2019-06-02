4c019d5bdb7b4aed2188c4edc792c0224c261b98
mrfong <therealshadix@gmail.com>
2019-06-02 16:04:56 +0300

    added new routes

--- work-task/src/router.js
+++ work-task/src/router.js
@@ -1,26 +1,41 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
 
 Vue.use(Router);
 
 export default new Router({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: Home
+  routes: [{
+      path: '/',
+      name: 'home',
+      component: Home,
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
-    }
-  ]
+      path: '/posts',
+      name: 'posts',
+      component: () => import('./views/posts.vue'),
+    },
+    {
+      path: '/post/:id',
+      name: 'post',
+      component: () => import('./views/post.vue'),
+    },
+    {
+      path: '/post/:id/comments',
+      name: 'comments',
+      component: () => import('./views/comments.vue'),
+    },
+    {
+      path: '/comments/postId=:id',
+      name: 'postcomments',
+      component: () => import('./views/postcomments.vue'),
+    },
+    {
+      path: '/posts/userId=:id',
+      name: 'usercomments',
+      component: () => import('./views/userposts.vue'),
+    },
+  ],
 });
