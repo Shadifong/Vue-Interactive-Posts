d2741b84e7fbea6ace63498d6886a693ba308b83
mrfong <therealshadix@gmail.com>
2019-06-02 16:24:40 +0300

    added a singular page for the post

---
+++ work-task/src/views/post.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="main-container">
+    <post
+      :title="post.title"
+      :body="post.body"
+      :userId="post.userId"
+      :postId="post.id"
+      :main="false"
+    />
+  </div>
+</template>
+
+<script>
+import post from "@/components/post.vue";
+const fetch = window.location.pathname.slice(7, 8);
+export default {
+  data() {
+    return {
+      post: []
+    };
+  },
+  methods: {
+    getPost() {
+      this.$http
+        .get(`https://jsonplaceholder.typicode.com/posts/${fetch}`)
+        .then(
+          function(response) {
+            this.post = response.data;
+          },
+          function(error) {
+            return error.statusText;
+          }
+        );
+    }
+  },
+  created() {
+    this.getPost();
+  },
+  name: "home",
+  components: {
+    post
+  }
+};
+</script>
+
+<style>
+.main-container {
+  margin: 0 auto;
+}
+</style>
