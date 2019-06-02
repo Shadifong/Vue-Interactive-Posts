794333df9710614b1cb8ca3de1203d966178bb18
mrfong <therealshadix@gmail.com>
2019-06-02 23:43:54 +0300

    added responsivneess for the searchbar

--- work-task/src/views/Home.vue
+++ work-task/src/views/Home.vue
@@ -6,25 +6,83 @@
   </div>
 </template>
 
- <style lang="sass" >
-body
-  font-family: Arial, sans-serif
-  background: url("http://www.inggez.com/img/bg-pattern.png"), linear-gradient(to left, #35495E, #47B882)
-  background-attachment: fixed
-  font-size: 16px
-  color: #171a1c
-  line-height: 1.5
+<style lang="scss">
+$color_pickled_bluewood_approx: #35495e;
+$color_ocean_green_approx: #47b882;
+$color_aztec_approx: #171a1c;
+$white: #fff;
+$color_alto_approx: #ddd;
 
-input
-  font-size: 18px
-  outline: none
-  padding: 80px
-  display: inline-block
-  width: 60%
-  border-radius: 6px
-  padding: 16px 70px 8px 30px
-  position: relative
-  background: #fff
-  border: 1px solid #ddd
+
+$font_0: Arial;
+$font_1: sans-serif;
+
+
+$url_0: url(http://www.inggez.com/img/bg-pattern.png);
+
+body {
+	font-family: $font_0, $font_1;
+	background: $url_0, linear-gradient(to left, $color_pickled_bluewood_approx, $color_ocean_green_approx);
+	background-attachment: fixed;
+	font-size: 16px;
+	color: $color_aztec_approx;
+	line-height: 1.5;
+}
+input {
+	font-size: 18px;
+	outline: none;
+	padding: 80px;
+	display: inline-block;
+	width: 26%;
+	border-radius: 8px;
+	padding: 16px 70px 8px 30px;
+	position: relative;
+	background: $white;
+	border: 1px solid $color_alto_approx;
+}
+
+@media only screen and (max-width: 600px) {
+ input{
+   width: 80%;
+  }
+}
 
 </style>
+
+
+
+
+<script>
+import post from "@/components/post.vue";
+import { compileFunction } from 'vm';
+
+export default {
+  data() {
+    return {
+      posts: [],
+
+    };
+  },
+  methods: {
+    getPosts() {
+      this.$http.get("https://jsonplaceholder.typicode.com/posts").then(
+        function(response) {
+          this.posts = response.data;
+          // console.log("data is",this.posts[0].title)
+        },
+        function(error) {
+          return error.statusText;
+        }
+      );
+    }
+  },
+  created() {
+        this.getPosts();
+
+},
+  name: "Home",
+  components: {
+    post
+  }
+};
+</script>
