6f033e8f22dc627df5f965ad4e0a23df85014e3c
mrfong <therealshadix@gmail.com>
2019-06-02 16:05:39 +0300

    imported vueresource,vuteify

--- work-task/src/main.js
+++ work-task/src/main.js
@@ -1,12 +1,21 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue';
+import './plugins/vuetify';
+import vueResource from 'vue-resource';
+import Vuetify from 'vuetify';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+Vue.use(vueResource);
+Vue.use(Vuetify);
 
 Vue.config.productionTip = false;
 
 new Vue({
+  created() {
+
+  },
   router,
   store,
-  render: h => h(App)
-}).$mount("#app");
+  render: h => h(App),
+}).$mount('#app');
